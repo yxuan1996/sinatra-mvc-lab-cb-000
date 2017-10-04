2832039f69aaee9d0951ed2640e82fa0f6b54699
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-04 22:29:27 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,19 +5,22 @@ class PigLatinizer
 
     non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
     vowels = ["a", "e", "i", "o", "u"]
+    answer = nil
 
     if non_pig_latin_words.include?(word)
-      word
+      answer = word
     elsif vowels.include? word[0]
       word << "ay"
+      answer = word
     else
       consonants = ""
       while !vowels.include?(word[0])
         consonants << word[0]
         word = word.split("")[1..-1].join
       end
-      word + consonants + 'ay'
+      answer = word + consonants + 'ay'
     end
+    answer
   end
 
 
